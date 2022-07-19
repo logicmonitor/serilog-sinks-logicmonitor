b316d3b997b189c3b8351150ac9f2415c3414990
AakashKhopade <aakash.khopade@logicmonitor.com>
2022-07-19 15:16:03 +0530

    Serilog Sink To write logs at Logicmonitor

---
+++ LoggerConfigurationsLMExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Serilog.Configuration;
+using Serilog.Events;
+using configuration = LogicMonitor.DataSDK.Configuration;
+using LogicMonitor.DataSDK.Model;
+using Serilog.Sinks.LogicMonitor;
+
+namespace Serilog
+{
+    public static class LoggerConfigurationLMExtensions
+    {
+        public static LoggerConfiguration LogicMonitor(
+            this LoggerSinkConfiguration loggerSinkConfiguration,
+            configuration configuration = null,
+            Resource resource = null,
+            bool batch = true,
+            int interval = 10,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum
+            )
+        {
+            var lmSink = new LogicmonitorSink(configuration, resource, batch, interval);
+            return loggerSinkConfiguration.Sink(lmSink, restrictedToMinimumLevel);
+        }
+    }
+}
---
+++ LogicmonitorSink.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Serilog.Events;
+using LogicMonitor.DataSDK.Api;
+using LogicMonitor.DataSDK.Model;
+using LogicMonitor.DataSDK;
+using conf = LogicMonitor.DataSDK.Configuration;
+using Serilog.Core;
+
+namespace Serilog.Sinks.LogicMonitor
+{
+    public class LogicmonitorSink : ILogEventSink
+    {
+        Logs Logs;
+        conf Configuration;
+        Resource Resource;
+
+        public LogicmonitorSink(conf conf = null, Resource resource = null, bool batch = true, int interval = 10)
+        {
+            Configuration = conf ??= new conf();
+            ApiClient apiClient = new ApiClient(Configuration);
+            Resource = resource ??= new Resource();
+            Logs = new Logs(batch: batch, interval: interval, apiClient: apiClient);
+
+        }
+
+        /// <summary>
+        /// Emit a batch of log events to LogicMonitor platform.
+        /// </summary>
+        /// <param name="events">The events to emit.</param>
+        public void Emit(LogEvent logEvent)
+        {
+            var response = Logs.SendLogs(message: logEvent.MessageTemplate.Text, resource: Resource);
+        }
+
+        
+
+    }
+}
